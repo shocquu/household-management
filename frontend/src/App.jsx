ab45cb7cf49f704d86be973040dd845915b2d53d
Shocky <mario123.al@gmail.com>
2022-12-19 00:47:40 +0100

    Init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Container from '@mui/material/Container';
+import SignIn from './pages/signIn/SignIn';
+
+export default function App() {
+    return (
+        <Container maxWidth='sm'>
+            <SignIn />
+        </Container>
+    );
+}
